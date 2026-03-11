cf6f3300b501f4937a355a386d01a5e46dfcea61
liandrew5448 <liandrew5448@gmail.com>
2026-03-11 12:45:10 -0700

    started cross validation, forward selection, and backward elimination

--- nearestSearch.h
+++ nearestSearch.h
@@ -8,4 +8,7 @@ public:
     int accuracy(const std::vector<std::vector<int>>& data, int current_set, int features_to_add);
     void search(const std::vector<std::vector<int>>& data, int current_set, int features_to_add);
     double distance(const std::vector<int>& instance1, const std::vector<int>& instance2);
+    void crossValidation(const std::vector<std::vector<int>>& data, int features_to_add);
+    void forwardSelection(const std::vector<std::vector<int>>& data);
+    void backwardElimination(const std::vector<std::vector<int>>& data);
 };
